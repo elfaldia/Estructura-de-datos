08460583ab6fb0af5346c5fa75fd1a04fba29174
elfaldia <118287825+elfaldia@users.noreply.github.com>
2023-10-02 23:26:36 +0000

    .

--- Pilas y colas/pilaEjercicio.cpp
+++ Pilas y colas/pilaEjercicio.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <stack>
-#include <cstdlib>
+#include <random> 
 #include <string>
 using namespace std;
 
@@ -22,22 +22,49 @@ int Camion::getPeso(){
 
 int main(){
 
-    int cant = 6 + rand() %10;
+    random_device rd;
+    mt19937 gen(rd());
+    uniform_int_distribution<int> dis(0, 49);
+
+    int cant = 6 + dis(gen) %10;
     int valor;
     stack<Camion> camiones,aux1,aux2;
-    for(int i = 0; i < cant;i++){       
-        camiones.push(Camion(rand()%50,to_string(i)));
+    for(int i = 0; i < cant;i++){
+      
+        camiones.push(Camion(dis(gen),to_string(i)));      
         cout<<camiones.top().getPeso()<<endl;
     }
-
+    cout<<" "<<endl;
     while(!camiones.empty()){
-        
-        if(camiones.top().getPeso() <= aux1.top().getPeso() || aux1.empty()){
-            Camion n = camiones.top();
-            aux1.push(n);
+        if(aux1.empty()){
+            aux1.push(camiones.top());
             camiones.pop();
+        }else{
+            if(camiones.top().getPeso() <= aux1.top().getPeso()){
+                aux1.push(camiones.top());
+                camiones.pop();
+            }else{
+                while(camiones.top().getPeso() >= aux1.top().getPeso() && !aux1.empty()){
+                    aux2.push(aux1.top());
+                    aux1.pop();           
+                }
+                aux1.push(camiones.top());
+                camiones.pop();
+                while(!aux2.empty()){
+                    aux1.push(aux2.top());
+                    aux2.pop();
+                }
+            }    
         }
-        
+    }
+    
+    if(camiones.empty()){
+        cout<<"hola";
+    }
+
+    while(!aux1.empty()){
+        camiones.push(aux1.top());
+        aux1.pop();
     }
 
     return 0;
